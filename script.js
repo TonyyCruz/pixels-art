ab8fedea67f3d2cd7687a8a935b73a8d0df3dea8
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-02-15 18:39:24 -0300

    ex 6 completo

--- script.js
+++ script.js
@@ -2,8 +2,48 @@ let colorSelected = '';
 
 window.onload = function() {
   colorSelected = 'black';
+  let painting = document.getElementById(colorSelected);
+  painting.classList.add('selected');
+  console.log(painting);
 };
+let removeSelected = document.getElementsByClassName('selected');
 
-function colorChange(event) {
-  event.target.style.backgoundColor = colorSelected;
+let blackSelected = document.querySelector('#black');
+blackSelected.addEventListener('click', colorBlack);
+function colorBlack() {
+  removeSelected[0].classList.remove('selected');
+  colorSelected = 'black';
+  blackSelected.classList.add('selected');
 }
+
+let redSelected = document.querySelector('#red');
+redSelected.addEventListener('click', colorRed);
+function colorRed() {
+  removeSelected[0].classList.remove('selected');
+  redSelected.classList.add('selected');
+  colorSelected = 'red';
+}
+
+let blueSelected = document.querySelector('#blue');
+blueSelected.addEventListener('click', colorblue);
+function colorblue() {
+  removeSelected[0].classList.remove('selected');
+  blueSelected.classList.add('selected');
+  colorSelected = 'blue';
+}
+
+let greenSelected = document.querySelector('#green');
+greenSelected.addEventListener('click', colorgreen);
+function colorgreen() {
+  removeSelected[0].classList.remove('selected');
+  greenSelected.classList.add('selected');
+  colorSelected = 'green';
+}
+
+let paint = document.querySelector('#pixel-board');
+paint.addEventListener('click', colorChange);
+function colorChange(event) {
+  event.target.style.backgroundColor = colorSelected;
+  console.log(event.target);
+}
+
