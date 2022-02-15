962203670868e5296791a76914fd3601ceeb1993
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-02-15 19:22:23 -0300

    sei la o que deu aqui

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 let colorSelected = '';
 
-window.onload = function() {
+window.onload = function () {
   colorSelected = 'black';
-  let painting = document.getElementById(colorSelected);
+  const painting = document.getElementById(colorSelected);
   painting.classList.add('selected');
 };
-let removeSelected = document.getElementsByClassName('selected');
+const removeSelected = document.getElementsByClassName('selected');
 
-let blackSelected = document.querySelector('#black');
-blackSelected.addEventListener('click', colorBlack);
 function colorBlack() {
   removeSelected[0].classList.remove('selected');
   colorSelected = 'black';
   blackSelected.classList.add('selected');
 }
+const blackSelected = document.querySelector('#black');
+blackSelected.addEventListener('click', colorBlack);
 
-let redSelected = document.querySelector('#red');
-redSelected.addEventListener('click', colorRed);
 function colorRed() {
   removeSelected[0].classList.remove('selected');
   redSelected.classList.add('selected');
   colorSelected = 'red';
 }
+const redSelected = document.querySelector('#red');
+redSelected.addEventListener('click', colorRed);
 
-let blueSelected = document.querySelector('#blue');
-blueSelected.addEventListener('click', colorblue);
 function colorblue() {
   removeSelected[0].classList.remove('selected');
   blueSelected.classList.add('selected');
   colorSelected = 'blue';
 }
+const blueSelected = document.querySelector('#blue');
+blueSelected.addEventListener('click', colorblue);
 
-let greenSelected = document.querySelector('#green');
-greenSelected.addEventListener('click', colorgreen);
 function colorgreen() {
   removeSelected[0].classList.remove('selected');
   greenSelected.classList.add('selected');
   colorSelected = 'green';
 }
+const greenSelected = document.querySelector('#green');
+greenSelected.addEventListener('click', colorgreen);
 
-let paint = document.querySelector('#pixel-board');
+const paint = document.querySelector('#pixel-board');
 paint.addEventListener('click', colorChange);
 function colorChange(event) {
   event.target.style.backgroundColor = colorSelected;
