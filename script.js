7ebf4b783349cf7d52c107fa8a45e728b119fc41
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-02-17 21:12:33 -0300

    atualizacao ex 10

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
+//salva a cor selecionada <===
 let colorSelected = '';
 
+//quando carrega a pagina define a cor selecionada como preto <===
 window.onload = function () {
   colorSelected = 'black';
   const painting = document.getElementById(colorSelected);
   painting.classList.add('selected');
 };
+
+//aponta salva quem esta com a classe 'selected', que sera a cor usada <===
 const removeSelected = document.getElementsByClassName('selected');
 
+
+//define a cor como preto <===
 function colorBlack() {
   removeSelected[0].classList.remove('selected');
   colorSelected = 'black';
   blackSelected.classList.add('selected');
 }
+
+//seleciona a cor como preto <===
 const blackSelected = document.querySelector('#black');
 blackSelected.addEventListener('click', colorBlack);
 
+//define a cor como vermelho <===
 function colorRed() {
   removeSelected[0].classList.remove('selected');
   redSelected.classList.add('selected');
   colorSelected = 'red';
 }
+
+//seleciona a cor como vermelho <===
 const redSelected = document.querySelector('#red');
 redSelected.addEventListener('click', colorRed);
 
+//define a cor como azul <===
 function colorblue() {
   removeSelected[0].classList.remove('selected');
   blueSelected.classList.add('selected');
   colorSelected = 'blue';
 }
+
+//seleciona a cor azul <===
 const blueSelected = document.querySelector('#blue');
 blueSelected.addEventListener('click', colorblue);
 
+//define a cor como verde
 function colorgreen() {
   removeSelected[0].classList.remove('selected');
   greenSelected.classList.add('selected');
   colorSelected = 'green';
 }
+
+//seleciona a cor verde <===
 const greenSelected = document.querySelector('#green');
 greenSelected.addEventListener('click', colorgreen);
 
+//pinta os blocos com a cor selecionada<===
 const paint = document.querySelector('#pixel-board');
 paint.addEventListener('click', colorChange);
 function colorChange(event) {
@@ -47,46 +65,97 @@ function colorChange(event) {
   }
 }
 
+//chama a funçao para limpar os blocos <===
 const clearButton = document.querySelector('#clear-board');
 const allBoxArray = paint.children;
 clearButton.addEventListener('click', clearAll);
+
+//pinta todos os blocos de branco <===
 function clearAll() {
   for (let i = 0; i < allBoxArray.length; i += 1) {
     allBoxArray[i].style.backgroundColor = 'white';
   }
 }
 
-// let boardSize = document.querySelector('#board-size').value;
-const boxFlex = document.querySelector('#board-box-flex');
-const generateBoard = document.querySelector('#generate-board');
-generateBoard.addEventListener('click', makeBox)
+//numero de blocos colocado no input e sua altura<===
+let boardSize = 0;
+let blockHeight = 0;
 
-function makeBox() {
-  let boardSize = document.querySelector('#board-size').value;
-  if (boardSize === '' || boardSize < 5 || boardSize > 50) {
-    window.alert('Board inválido!');
-  } else {
-    for (let i = 0; i < (boardSize ** 2); i += 1) {
-      let block = document.createElement('div');
-      block.classList.add('block');
-      boxFlex.appendChild(block);
-    }
-    boxDesign(boardSize);
+//apaga todos os blocos <===
+function  clearBox() {
+  let dellPixel = allBoxArray.length;
+  for (let i = 0; i < dellPixel; i += 1) {
+    paint.removeChild(allBoxArray[0])
   }
 }
-const boxFlexFather = document.querySelector('#board-box-flex');
-function boxDesign(boardSize) {
-let block = boxFlexFather.children;
-let blocksize = (399 - (2 * boardSize)) / boardSize;
-console.log(boardSize);
-console.log(blocksize);
-  for (let i = 0; i < block.length; i += 1) {
-    block[i].style.border = '1px solid black';
-    block[i].style.height = `${blocksize}px`;
-    block[i].style.width = `${blocksize}px`;
-    block[i].style.backgroundColor = 'white';
-    block[i].style.display = 'inline-block';
-    // block[i].style.marginTop = `-10%`;
-    
+
+//define o tamanho dos blocos <===
+function addBlockBox() {
+  if (boardSize <= 15) {
+    blockHeight = 40; }
+    else if ( boardSize > 15 && boardSize <= 30) {
+      blockHeight = 25; }
+      else { blockHeight = 10;
   }
 }
+//adiciona os blocos <===
+function addblockToBox() {
+  for (let i = 0; i < (boardSize ** 2); i += 1) {
+    let block = document.createElement('div');
+    paint.appendChild(block);
+  }
+}
+
+//ajusta o tamanho dos blocos <===
+function blockAdjust() {
+  for (let i = 0; i < paint.children.length; i += 1) {
+    paint.children[i].style.height = `${blockHeight}px`;
+    paint.children[i].style.width = `${blockHeight}px`;
+    paint.children[i].style.display = 'inline-block';
+    paint.children[i].style.backgroundColor = 'white';
+    paint.children[i].style.border = '1px solid black'
+    paint.children[i].style.borderRadius = '10%'
+    paint.children[i].style.marginRight = '4px';
+  }
+}
+
+//ajusta o tamanho do box(container de blocos) maior que 30 <===
+function boxAdjustHightNumber() {
+  let boxHeight = boardSize * blockHeight + (12 * boardSize + (boardSize / 2));
+  let boxWidth = boardSize * blockHeight + (6 * boardSize);
+  paint.style.height = `${boxHeight}px`
+  paint.style.width = `${boxWidth}px`
+}
+
+//ajusta o tamanho do box(container de blocos) até 30 <===
+function boxAdjust() {
+  if (boardSize > 30) { boxAdjustHightNumber() }
+  else {
+    let boxHeight = boardSize * blockHeight + (6 * boardSize + (boardSize / 2));
+    let boxWidth = boardSize * blockHeight + (6 * boardSize);
+    paint.style.height = `${boxHeight}px`
+    paint.style.width = `${boxWidth}px`
+  }
+}
+
+//verifica se o valor input é valido <===
+function inputCheque() {
+  if (boardSize === '') {
+    window.alert('')
+  }
+}
+
+function generateBox() {
+boardSize = Number(document.querySelector('#board-size').value);
+inputCheque();
+  clearBox();
+  addBlockBox();
+  addblockToBox();
+  blockAdjust();
+  boxAdjust();
+}
+
+//chama a funçao para gerar o quadro com o input number <===
+const generateBoard = document.querySelector('#generate-board');
+generateBoard.addEventListener('click', generateBox);
+
